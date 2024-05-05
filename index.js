e3cddc282f81f226202c1e2d6b37a70b303616cd
rosamondlie <rosamond.patricia.01@gmail.com>
2024-05-05 22:48:05 +0700

    second commit

--- index.js
+++ index.js
@@ -9,3 +9,47 @@ function togglePopup2(){
 function togglePopup3(){
     document.getElementById("popup-3").classList.toggle("active");
 }
+
+
+
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyDNaFocwOFh7Gu_Jjfs8WFsdkZ4LjWHc_Q",
+    authDomain: "puddingfest-9f30d.firebaseapp.com",
+    projectId: "puddingfest-9f30d",
+    storageBucket: "puddingfest-9f30d.appspot.com",
+    messagingSenderId: "138766887587",
+    appId: "1:138766887587:web:328af1405c6c855d9bad16",
+    measurementId: "G-QMWEKV4YQ0"
+});
+
+const db = firebaseApp.firestore();
+
+const saveData = () => {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const telp_num = document.getElementById('telp_num').value;
+    const event = document.getElementById('event').value;
+
+    db.collection('audience')
+    .add({
+        name: name,
+        email: email,
+        telp_num: telp_num,
+        event: event
+    })
+    .then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch((eror) => {
+        console.log("Error adding document: ", eror);
+    });
+}
+
+
+// let getData = document.getElementById("getData");
+
+// getData.addEventListener("click",()=>{
+//     let xhr =new XMLHttpRequest();
+//     xhr.open('GET',)
+// })
